eeaf2588455194837f23232a761708e03d394708
Nindya Fathul Risya <79150872+ninfrrr@users.noreply.github.com>
2022-09-28 22:22:22 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,30 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
 
-//Route files
-const menu = require('./routes/menu');
-
-//Load env vars
 dotenv.config({path: './config/config.env'});
 
 const app = express();
-
-//Custom logging middleware (unused)
-//app.use(logger);
-
-//Using Morgan instead (dev logging middleware)
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
-
-//Mount routers
-app.use('/api/v1/menu', menu);
-
 const PORT = process.env.PORT || 5000;
-
-app.listen(
-    PORT, 
-    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
-
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
