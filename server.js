8886915adbb14dca8bb7ceffac09dffd62f4ff49
Daffa Muhammad Romero <70407002+daffaromero@users.noreply.github.com>
2022-09-29 12:28:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db')
 
+//Load env vars
 dotenv.config({path: './config/config.env'});
 
+//Connect to database
+connectDB();
+
+//Route files
+const menu = require('./routes/menu');
+
 const app = express();
+
+//Body Parser
+app.use(express.json())
+
+//Custom logging middleware (unused)
+//app.use(logger);
+
+//Using Morgan instead (dev logging middleware)
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+//Mount routers
+app.use('/api/v1/menu', menu);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+app.listen(
+    PORT, 
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
