a78a24a0495b7f437f3f744e3c3ed67d5168e648
nrwfebriani <nur.wulan.febriani@mail.ugm.ac.id>
2022-09-29 18:53:18 +0700

    Menambahkan custom error handling, mongoose middleware slugify

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,46 +1,68 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const Menu = require('../models/Menu');
 
 // @desc    Get all menu items
 // @route   GET /api/v1/menu
 // @access  Public
-exports.getAllMenu = (req, res, next) => {
-    res
-        .status(200)
-        .json({success: true, msg: 'Show all menu items'});
-}
+exports.getAllMenu = asyncHandler(async (req, res, next) => {
+        const menus = await Menu.find();
+        res
+            .status(200)
+            .json({success: true, count: menus.length, data: menus});
+});
 
 // @desc    Get a single menu item
 // @route   GET /api/v1/menu/:id
 // @access  Public
-exports.getMenu = (req, res, next) => {
-    res
-        .status(200)
-        .json({success: true, msg: `Show menu with ID ${req.params.id}`});
-}
+exports.getMenu = asyncHandler(async (req, res, next) => {
+        const menu = await Menu.findById(req.params.id);
+        if(!menu){
+            return next(new ErrorResponse(`Resource not found with ID of ${req.params.id}`, 404));
+        }
+        res
+            .status(200)
+            .json({success: true, data: menu});
+});
 
 // @desc    Create new menu item
 // @route   POST /api/v1/menu
 // @access  Private
-exports.createMenu = (req, res, next) => {
-    res
-        .status(200)
-        .json({success: true, msg: 'Create new menu item'});
-}
+exports.createMenu = asyncHandler(async (req, res, next) => {
+        const menu = await Menu.create(req.body);
+        res
+            .status(201)
+            .json({
+                success: true,
+                data: menu
+            });
+});
 
 // @desc    Update a single menu item
 // @route   PUT /api/v1/menu/:id
 // @access  Private
-exports.updateMenu = (req, res, next) => {
-    res
+exports.updateMenu = asyncHandler(async (req, res, next) => {
+        const menu = await Menu.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if(!menu){
+            return next(new ErrorResponse(`Resource not found with ID of ${req.params.id}`, 404));
+        }
+        res
         .status(200)
-        .json({success: true, msg: `Update menu item with ID ${req.params.id}`});
-}
+        .json({success: true, data: menu});
+});
 
 // @desc    Delete menu item
 // @route   DELETE /api/v1/menu/:id
 // @access  Private
-exports.deleteMenu = (req, res, next) => {
-    res
+exports.deleteMenu = asyncHandler(async (req, res, next) => {
+        const menu = await Menu.findByIdAndDelete(req.params.id);
+        if(!menu){
+            return next(new ErrorResponse(`Resource not found with ID of ${req.params.id}`, 404));
+        }
+        res
         .status(200)
-        .json({success: true, msg: `Delete menu item ${req.params.id}`})    
-}
-
+        .json({success: true, data: {}});
+});
---
+++ models/Menu.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const MenuSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true
+    },
+    slug: String,
+    description:{
+        type: String,
+        maxlength: [500, 'Description cannot be more than 500 characters']
+    },
+    image:{
+        type: String,
+        default: 'no-image.jpg'
+    },
+    price:{
+        type: Number,
+        required: [true, 'Please add a price']
+    },
+    available:{
+        type: Boolean,
+        required: true,
+        default: true
+    }
+});
+
+// Create menu slug from the name
+
+MenuSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, {lower: true});
+    next();
+});
+
+module.exports = mongoose.model('Menu', MenuSchema);
