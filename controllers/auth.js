ba1b53041ecbe7cc99b1bd19b343ee202a2e9682
ninfrrr <nfrisya85@gmail.com>
2022-11-07 14:55:14 +0700

    sending JWT in a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,8 @@ exports.register = asyncHandler(async(req, res, next) =>{
         role
     });
 
-    //create token
-    const token = user.getSignedJwtToken();
+    sendTokenResponse(user, 200, res);
 
-    res.status(200).json({ success: true, token });
 });
 
 // @desc    login user
@@ -45,7 +43,29 @@ exports.login = asyncHandler(async(req, res, next) =>{
     }
 
     //create token
+    sendTokenResponse(user, 200, res);
+});
+
+// get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) =>{
+    //create token
     const token = user.getSignedJwtToken();
 
-    res.status(200).json({ success: true, token });
-});
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    if(process.env.NODE_ENV === 'production'){
+        options.secure = true;
+    }
+
+    res
+        .status(statusCode)
+        .cookie('token', token, options)
+        .json({
+            success: true,
+            token
+        })
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
@@ -19,7 +20,10 @@ const auth = require('./routes/auth');
 const app = express();
 
 //Body Parser
-app.use(express.json())
+app.use(express.json());
+
+//cookie parser
+app.use(cookieParser());
 
 //Custom logging middleware (unused)
 //app.use(logger);
