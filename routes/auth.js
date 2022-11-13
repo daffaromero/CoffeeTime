6612bf001f324c1609c4961140e795c023d280ed
ninfrrr <nfrisya85@gmail.com>
2022-11-14 00:00:12 +0700

    update user details

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,9 @@ const {
     login, 
     getMe, 
     forgotPassword,
-    resetPassword } = require('../controllers/auth');
+    resetPassword,
+    updateDetails,
+    updatePassword } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -12,7 +14,9 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
