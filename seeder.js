a540b337a74f80f2f7e4cc7f73a9f71753249b93
ninfrrr <nfrisya85@gmail.com>
2022-11-13 22:20:48 +0700

    user relationship

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({ path:'./config/config.env'});
 
 //Load models
 const Menu = require('./models/Menu');
+const User = require('./models/User');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,10 +19,15 @@ mongoose.connect(process.env.MONGO_URI, {
 const menus = JSON.parse(fs.readFileSync(`${__dirname}/_data/menu.json`,`utf-8`)
 );
 
+const user = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 //Import into DB
 const importDB = async() => {
     try{
         await Menu.create(menus);
+        await User.create(users);
         console.log('Database succsessfully imported..'.green.inverse);
         process.exit();
     }
@@ -34,6 +40,7 @@ const importDB = async() => {
 const deleteDB = async() => {
     try{
         await Menu.deleteMany();
+        await User.deleteMany();
         console.log('Database destroyed..'.red.inverse);
         process.exit();
     }
