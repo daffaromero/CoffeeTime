21616a7a53a79ffff8e50f6b778373dfe2f13c8a
daffaromero <daffaromero@mail.ugm.ac.id>
2022-11-16 22:15:21 +0700

    many many fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,46 +1,50 @@
-const jwt = require('jsonwebtoken');
-const asyncHandler = require('./async');
-const ErrorResponse = require('../utils/errorResponse');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("./async");
+const ErrorResponse = require("../utils/errorResponse");
+const User = require("../models/User");
 
 //protext routes
 exports.protect = asyncHandler(async (req, res, next) => {
-    let token;
+  let token;
 
-    if(
-        req,headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-    ){
-        token = req.headers.authorization.split(' ')[1];
-    }
-    // else if (req.cookies.token){
-    //     token = req.cookies.token
-    // }
-    //make sure token exists
-    if(!token){
-        return next(new ErrorResponse('not authorized to access this route', 401));
-    }
-    try {
-       //verify token
-       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-       
-       console.log(decoded);
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  // else if (req.cookies.token){
+  //     token = req.cookies.token
+  // }
+  //make sure token exists
+  if (!token) {
+    return next(new ErrorResponse("not authorized to access this route", 401));
+  }
+  try {
+    //verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-       req.user = await User.findById(decoded.id);
+    console.log(decoded);
 
-       next();
-    } catch (error) {
-        return next(new ErrorResponse('not authorized to access this route', 401));
+    req.user = await User.findById(decoded.id);
 
-    }
-})
+    next();
+  } catch (error) {
+    return next(new ErrorResponse("not authorized to access this route", 401));
+  }
+});
 
 //grant access to spesific roles
 exports.authorize = (...roles) => {
-    return (req, res, next) => {
-        if(!roles.includes(req.user.role)){
-            return next(new ErrorResponse('user role ${req.user.role} is not authorized to access this route', 403));
-        }
-        next();
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          "user role ${req.user.role} is not authorized to access this route",
+          403
+        )
+      );
     }
-}
+    next();
+  };
+};
--- seeder.js
+++ seeder.js
@@ -1,51 +1,58 @@
-const fs = require('fs');
-const mongoose = require('mongoose');
-const colors = require('colors');
-const dotenv = require('dotenv')
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config({ path:'./config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 //Load models
-const Menu = require('./models/Menu');
+const Menu = require("./models/Menu");
+const User = require("./models/User");
+
+//Load sample data
+const menus = require("./data/menus");
+const users = require("./data/users");
 
 //Connect to DB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology:true
-});
-
-//Read JSON files
-const menus = JSON.parse(fs.readFileSync(`${__dirname}/_data/menu.json`,`utf-8`)
-);
+const connectDB = require("./config/db");
+connectDB();
 
 //Import into DB
-const importDB = async() => {
-    try{
-        await Menu.create(menus);
-        console.log('Database succsessfully imported..'.green.inverse);
-        process.exit();
-    }
-    catch(err){
-        console.error(err);
-    }
-}
+const importDB = async () => {
+  try {
+    await Menu.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+    const adminUser = createdUsers[0]._id;
+    const sampleMenus = menus.map((menu) => {
+      return { ...menu, user: adminUser };
+    });
+
+    await Menu.insertMany(sampleMenus);
+
+    console.log("Database succsessfully imported!");
+    process.exit();
+  } catch (err) {
+    console.error(`${err}`);
+    process.exit(1);
+  }
+};
 
 //Delete data
-const deleteDB = async() => {
-    try{
-        await Menu.deleteMany();
-        console.log('Database destroyed..'.red.inverse);
-        process.exit();
-    }
-    catch(err){
-        console.error(err);
-    }
+const deleteDB = async () => {
+  try {
+    await Menu.deleteMany();
+    await User.deleteMany();
+    console.log("Database destroyed.".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 //Apabila mengetik node seeder -i untuk mengimport data dan node seeder -d untuk menghapus semua data
-if(process.argv[2] === '-i'){
-    importDB();
-}
-else if(process.argv[2] === '-d'){
-    deleteDB();
+if (process.argv[2] === "-i") {
+  importDB();
+} else if (process.argv[2] === "-d") {
+  deleteDB();
 }
