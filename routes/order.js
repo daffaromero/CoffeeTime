e41d24dcbe649d0f755fcd33da759198b1be2ee8
Daffa Romero <daffaromero@mail.ugm.ac.id>
2022-11-24 22:52:08 +0700

    Order Controller

---
+++ routes/order.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { protect, authorize } = require("../middleware/auth");
+
+const {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getMyOrders,
+  getOrders,
+} = require('../controllers/orderController');
+
+const router = express.Router()
+
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
+router.route('/myorders').get(protect, getMyOrders)
+router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, authorize("admin"), updateOrderToDelivered)
+
+module.exports = router;
