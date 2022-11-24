6a42c39b1403abb8bf437fd44225771a7a6327bb
ninfrrr <nindya.f.r@mail.ugm.ac.id>
2022-11-24 16:49:33 +0700

    backend fixes

--- controllers/auth.js
+++ controllers/auth.js
@@ -175,6 +175,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
   res.status(200).json({
+    success: true,
     data: user,
   });
 });
