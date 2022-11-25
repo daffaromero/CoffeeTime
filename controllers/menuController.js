22426495c4697be9937933744f90bc367a48686d
daffaromero <daffaromero@mail.ugm.ac.id>
2022-11-25 11:48:38 +0700

    undo search

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -35,8 +35,8 @@ exports.getAllMenu = asyncHandler(async (req, res, next) => {
   );
 
   //Finding menu
-  query = await Menu.find({ ...keyword });
-  //   query = Menu.find(JSON.parse(queryStr) || { ...keyword });
+  //   query = await Menu.find({ ...keyword });
+  query = Menu.find(JSON.parse(queryStr) || { ...keyword });
 
   //Select Fields
   if (req.query.select) {
