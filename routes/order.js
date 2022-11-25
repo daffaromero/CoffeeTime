85f58c295dfed4879df0436c396817a226262d66
daffaromero <daffaromero@mail.ugm.ac.id>
2022-11-25 08:25:04 +0700

    fix order route: authorize admin

--- routes/order.js
+++ routes/order.js
@@ -8,14 +8,19 @@ const {
   updateOrderToDelivered,
   getMyOrders,
   getOrders,
-} = require('../controllers/orderController');
+} = require("../controllers/orderController");
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
-router.route('/myorders').get(protect, getMyOrders)
-router.route('/:id').get(protect, getOrderById)
-router.route('/:id/pay').put(protect, updateOrderToPaid)
-router.route('/:id/deliver').put(protect, authorize("admin"), updateOrderToDelivered)
+router
+  .route("/")
+  .post(protect, addOrderItems)
+  .get(protect, authorize("admin"), getOrders);
+router.route("/myorders").get(protect, getMyOrders);
+router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+router
+  .route("/:id/deliver")
+  .put(protect, authorize("admin"), updateOrderToDelivered);
 
 module.exports = router;
