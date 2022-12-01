0b068777644f622957313ff2a04866d4584d425c
daffaromero <daffaromero@mail.ugm.ac.id>
2022-12-01 15:36:15 +0700

    try: enabling CORS requests 2

--- routes/menu.js
+++ routes/menu.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { protect, authorize } = require("../middleware/auth");
+const cors = require("cors");
 
 const {
   getAllMenu,
@@ -11,14 +12,17 @@ const {
 } = require("../controllers/menuController");
 
 const router = express.Router();
-router.route("/").get(getAllMenu).post(protect, authorize("admin"), createMenu);
+router
+  .route("/")
+  .get(cors(), getAllMenu)
+  .post(cors(), protect, authorize("admin"), createMenu);
 
 router
   .route("/:id")
-  .get(getMenu)
-  .put(protect, authorize("admin"), updateMenu)
-  .delete(protect, authorize("admin"), deleteMenu);
+  .get(cors(), getMenu)
+  .put(cors(), protect, authorize("admin"), updateMenu)
+  .delete(cors(), protect, authorize("admin"), deleteMenu);
 
 module.exports = router;
 
-router.route("/:id/photo").put(menuPhotoUpload);
+router.route("/:id/photo").put(cors(), menuPhotoUpload);
