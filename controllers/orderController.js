6b2aa5105cced03947fa4735c29cb7bb6b125abd
nrwfebriani <nur.wulan.febriani@mail.ugm.ac.id>
2022-12-02 11:08:48 +0700

    try: discard read email

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -59,7 +59,7 @@ exports.updateOrderToPaid = asyncHandler(async (req, res) => {
       id: req.body.id,
       status: req.body.status,
       update_time: req.body.update_time,
-      email_address: req.body.payer.email,
+      //email_address: req.body.payer.email,
     };
     const updatedOrder = await order.save();
 
