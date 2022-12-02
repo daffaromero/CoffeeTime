c7bbabd2da286f0a46ae0e95d79b0cb3108cf415
daffaromero <daffaromero@mail.ugm.ac.id>
2022-12-02 07:42:48 +0700

    Revert "try: enabling CORS requests 2"
    This reverts commit 0b068777644f622957313ff2a04866d4584d425c.

--- routes/menu.js
+++ routes/menu.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { protect, authorize } = require("../middleware/auth");
-const cors = require("cors");
 
 const {
   getAllMenu,
@@ -12,17 +11,14 @@ const {
 } = require("../controllers/menuController");
 
 const router = express.Router();
-router
-  .route("/")
-  .get(cors(), getAllMenu)
-  .post(cors(), protect, authorize("admin"), createMenu);
+router.route("/").get(getAllMenu).post(protect, authorize("admin"), createMenu);
 
 router
   .route("/:id")
-  .get(cors(), getMenu)
-  .put(cors(), protect, authorize("admin"), updateMenu)
-  .delete(cors(), protect, authorize("admin"), deleteMenu);
+  .get(getMenu)
+  .put(protect, authorize("admin"), updateMenu)
+  .delete(protect, authorize("admin"), deleteMenu);
 
 module.exports = router;
 
-router.route("/:id/photo").put(cors(), menuPhotoUpload);
+router.route("/:id/photo").put(menuPhotoUpload);
